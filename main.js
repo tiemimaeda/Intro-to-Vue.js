2e2aef0c305bafa7070cc2ba25cb5344b7fcea27
tiemimaeda <priscila.maeda@yahoo.com.br>
2020-04-14 00:51:18 -0300

    lesson 5

--- main.js
+++ main.js
@@ -4,17 +4,31 @@ var app = new Vue({
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
     inStock: true,
-    onSale: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
-        variantColor: "green"
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg'
       },
       {
         variantId: 2235,
-        variantColor: "blue"
-      },
-    ]
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    },
+    removeFromCart() {
+      this.cart -= 1
+    }
   }
 })
+
