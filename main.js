e077ee63d6d511fdd6f62bb70135d8a95a737adb
tiemimaeda <priscila.maeda@yahoo.com.br>
2020-04-13 23:50:54 -0300

    lesson 3

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg'
+    image: './assets/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    onSale: true
   }
 })
